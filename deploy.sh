108dc27a931ce838fd1952a9cf0a03b1e1fa4ec1
onehouwong <onehouwong@gmail.com>
2025-04-27 15:38:31 -0400

    remove sudo in deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sudo -E dms_cli install mobiflow-auditor 0.0.3 ricxapp
-sudo -E dms_cli install mobiflow-auditor 0.0.3 ricxapp charts/values.yaml
+dms_cli install mobiflow-auditor 0.0.3 ricxapp charts/values.yaml
